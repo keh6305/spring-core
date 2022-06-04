dee72e86d23060a3b82e95acde80110555824285
keh6305 <keh6305@gmail.com>
2022-06-04 22:24:41 +0900

    config 정리

--- src/main/java/hello/core/discount/AppConfig.java
+++ src/main/java/hello/core/discount/AppConfig.java
@@ -10,11 +10,21 @@ public class AppConfig
 {
     public MemberService memberService()
     {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository()
+    {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService()
     {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy()
+    {
+        return new FixDiscountPolicy();
     }
 }
