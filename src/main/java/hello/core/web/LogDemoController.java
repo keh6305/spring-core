bbd21ded32b0d39baeef2db64ef7ce809c9ecaba
keh6305 <keh6305@gmail.com>
2022-08-24 00:12:52 +0900

    request 스코프, 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,14 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController
 {
     private final LogDemoService logDemoService;
-//    private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @ResponseBody
     @RequestMapping("log-demo")
     public String logDemo(HttpServletRequest request) throws InterruptedException
     {
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());
+
+//        MyLogger myLogger = myLoggerProvider.getObject();
 
         String requestURL = request.getRequestURL().toString();
 
